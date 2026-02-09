e4ffcee7df967bc1fffd5324659edccc3348a1ee
Matematikoua <kevin.esmeral@ucaldas.edu.co>
2026-02-08 21:12:47 -0500

    agregue justificar texto sobre mi

--- components/About.js
+++ components/About.js
@@ -17,7 +17,7 @@ function About() {
             
             <div>
               <h3 className="text-2xl font-bold mb-4 text-[var(--primary-color)]">Bienvenidos!</h3>
-              <p className="text-lg text-[var(--text-secondary)] mb-6">
+              <p className="text-justify text-lg text-[var(--text-secondary)] mb-6">
                 Mi nombre es Kevin Esmeral García, colombiano de nacimiento y Matemático de profesión. 
 Actualmente soy docente tiempo completo y el líder del Grupo de Investigación en Estadística y Matemáticas de la Universidad de Caldas, además soy miembro activo del   Grupo de Investigación de Ecuaciones Diferenciales, Modelación y Simulación-GEDMyS, de la Universidad Pedagógica y Tecnológica de Colombia.
               </p>
@@ -29,7 +29,7 @@ Actualmente soy docente tiempo completo y el líder del Grupo de Investigación 
                   </div>
                   <div className="ml-4">
                     <h4 className="font-semibold text-lg mb-1">Formación Académica</h4>
-                    <p className="text-[var(--text-secondary)]">Egresado del programa de Matemáticas de la Universidad del Atlántico en Barranquilla, Colombia.  He realizado mis estudios de posgrado en México. Soy Magister en Ciencias Matemáticas del Instituto de Física y Matemáticas (IFM) de la Universidad Michoacana de San Nicolás de Hidalgo y del Centro de Ciencias Matemáticas (CCM) de la Universidad Nacional Autónoma de México (UNAM),    Doctor en Ciencias  en  la especialidad de Matemáticas del Centro de Investigación y de Estudios Avanzados del Instituto Politécnico Nacional (CINVESTAV-IPN).	</p>
+                    <p className="text-justify text-[var(--text-secondary)]">Egresado del programa de Matemáticas de la Universidad del Atlántico en Barranquilla, Colombia.  He realizado mis estudios de posgrado en México. Soy Magister en Ciencias Matemáticas del Instituto de Física y Matemáticas (IFM) de la Universidad Michoacana de San Nicolás de Hidalgo y del Centro de Ciencias Matemáticas (CCM) de la Universidad Nacional Autónoma de México (UNAM),    Doctor en Ciencias  en  la especialidad de Matemáticas del Centro de Investigación y de Estudios Avanzados del Instituto Politécnico Nacional (CINVESTAV-IPN).	</p>
                   </div>
                 </div>
                 
@@ -39,7 +39,7 @@ Actualmente soy docente tiempo completo y el líder del Grupo de Investigación 
                   </div>
                   <div className="ml-4">
                     <h4 className="font-semibold text-lg mb-1">Experiencia Investigativa</h4>
-                    <p className="text-[var(--text-secondary)]">Debido a mi formación matemática las áreas de mi interés son las C*-álgebras, álgebra y teoría de operadores de Toeplitz, Frames y Wavelets. Las investigaciones que he realizado se han enfocado en la descripción explicita de ciertas C*-álgebras conmutativas generadas por  operadores de  Toeplitz y en la descripción del comportamiento de los Frames (marcos)  en espacios con métrica indefinida. 
+                    <p className="text-justify text-[var(--text-secondary)]">Debido a mi formación matemática las áreas de mi interés son las C*-álgebras, álgebra y teoría de operadores de Toeplitz, Frames y Wavelets. Las investigaciones que he realizado se han enfocado en la descripción explicita de ciertas C*-álgebras conmutativas generadas por  operadores de  Toeplitz y en la descripción del comportamiento de los Frames (marcos)  en espacios con métrica indefinida. 
 </p>
                   </div>
                 </div>
@@ -50,7 +50,7 @@ Actualmente soy docente tiempo completo y el líder del Grupo de Investigación 
                   </div>
                   <div className="ml-4">
                     <h4 className="font-semibold text-lg mb-1">Experiencia docente</h4>
-                    <p className="text-[var(--text-secondary)]">He trabajado como docente en diferentes instituciones de educación superior tales como la Universidad Nacional de Colombia, sede Manizales, Universidad Pedagógica y Tecnológica de Colombia (UPTC) y  Universidad de Sucre,  en todas ellas impartiendo cursos de matemáticas a nivel de pregrado y posgrado. Mi enfoque pedagógico se centra en la claridad conceptual y el desarrollo del pensamiento crítico en mis estudiantes. Actualmente soy docente tiempo completo en la Universidad de Caldas en Manizales, Colombia.</p>
+                    <p className="text-justify text-[var(--text-secondary)]">He trabajado como docente en diferentes instituciones de educación superior tales como la Universidad Nacional de Colombia, sede Manizales, Universidad Pedagógica y Tecnológica de Colombia (UPTC) y  Universidad de Sucre,  en todas ellas impartiendo cursos de matemáticas a nivel de pregrado y posgrado. Mi enfoque pedagógico se centra en la claridad conceptual y el desarrollo del pensamiento crítico en mis estudiantes. Actualmente soy docente tiempo completo en la Universidad de Caldas en Manizales, Colombia.</p>
                   </div>
                 </div>
               </div>
